f42f014a222ab52e8408a2216af62c5a409ad940
LordSurya777 <nemmanisuryateja@gmail.com>
2026-03-16 13:55:14 +0530

    Initial commit

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,48 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+export const protectRoute = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+
+    if (!accessToken) {
+      console.log('No token provided');
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No access token provided" });
+    }
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.userId).select("-password");
+
+      if (!user) {
+        console.log('User not found');
+        return res.status(401).json({ message: "User not found" });
+      }
+
+      req.user = user;
+      console.log('Authentication Successful');
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        console.log('Token Expired');
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Access token expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    console.log("Error in protectRoute middleware", error.message);
+    return res
+      .status(401)
+      .json({ message: "Unauthorized - Invalid access token " });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin Only" });
+  }
+};
